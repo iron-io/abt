27030abf71e0756818d7c54d23f66ec85f3f69ee
rkononov <rkononov@gmail.com>
2012-06-08 20:50:22 +0600

    fixed git_url related error

--- lib/abt/notifiers/hip_chat_notifier.rb
+++ lib/abt/notifiers/hip_chat_notifier.rb
@@ -12,17 +12,20 @@ class HipChatNotifier
   def send_formatted_message(params)
     result = params[:result]
     color = result.passed? ? 'green' : 'red'
-    message = "URL: #{@git_url} Result: <b>#{result.status}</b> [#{result.pass_percentage}%] (#{params[:elapsed_time]} sec)<br>
+    message = "Result: <b>#{result.status}</b> [#{result.pass_percentage}%] (#{params[:elapsed_time]} sec)<br>
      <strong>#{result.run_count}</strong> tests, <strong>#{result.assertion_count}</strong> assertions"
     if result.error_occurred? || result.failure_occurred?
       message+=", <strong>#{result.failure_count}</strong> failures, <strong>#{result.error_count}</strong>, errors<br/>"
       result.faults.each { |f| message+="<br><pre>#{add_github_urls(f.to_s.gsub(/>/, ' ').gsub(/</, ' '))}</pre><br/>" }
     end
+    message+="<br/>URL: <b>#{@git_url}</b> "
     send_message(message, color)
   end
 
   def add_github_urls(message)
-    url = @git_url.gsub!(/git:/, 'https:').gsub!(/\.git/, '')
+    return message unless @git_url
+    puts "GIT_URL:#{@git_url}"
+    url = @git_url.gsub(/git:/, 'https:').gsub(/\.git/, '')
     pass = message.gsub(/\/cloned\/(lib\/.+?):(\d+):/,
                         "<a href='#{url}" +
                             "/blob/master" +
