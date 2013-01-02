1d7019c5ae09ed3a9f7ee8f319cc541ac9a06b70
Alexander Kolesen <akolesen@iron.io>
2013-01-03 02:53:30 +0300

    Switched to using short messages in important room in hipchat

--- notifiers/hip_chat_notifier.rb
+++ notifiers/hip_chat_notifier.rb
@@ -22,14 +22,20 @@ class HipChatNotifier
       result.faults.each { |f| message+="<br><pre>#{add_github_urls(f.to_s.gsub(/>/, ' ').gsub(/</, ' '))}</pre><br/>" }
     end
     message+="<br/>URL: <b>#{@git_url}</b> "
-    message+="<br/>HUD: <a href='#{hud_url(@iw_details)}'>#{@iw_details[:task_id]}</a> " if @iw_details
-    send_message(@room_name,message, color)
-    send_message(@important_room_name,message, color,true) unless result.passed?
+    message+="<br/>HUD (log): <a href='#{hud_log_url(@iw_details)}'>#{@iw_details[:task_id]}</a> " if @iw_details
+
+    important_room_failure_message = "Result: <b>#{result.status}</b> [#{result.pass_percentage}%] (#{params[:elapsed_time]} sec)<br>
+     <strong>#{result.run_count}</strong> tests, <strong>#{result.assertion_count}</strong> assertions"
+    important_room_failure_message += "<br/>URL: <b>#{@git_url}</b> "
+    important_room_failure_message += "<br/>HUD (log): <a href='#{hud_log_url(@iw_details)}'>#{@iw_details[:task_id]}</a> " if @iw_details    
+
+    send_message(@room_name, message, color)
+    send_message(@important_room_name, important_room_failure_message, color,true) unless result.passed?
   end
 
 
-  def hud_url(details)
-    "http://hud.iron.io/tq/projects/#{details[:project_id]}/jobs/#{details[:task_id]}"
+  def hud_log_url(details)
+    "http://hud.iron.io/tq/projects/#{details[:project_id]}/jobs/#{details[:task_id]}/log"
   end
 
   def format_benchmarks(test_benchmarks)
